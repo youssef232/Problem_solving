7da4e1351dc476b24d49840d0d9e27512ffcb6d3
ymost <ymostafa232@gmail.com>
2022-04-26 00:16:12 +0200

    day 3

---
+++ 976. Largest Perimeter Triangle/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace MyApp
+{
+    internal class Program
+    {
+        public static void Main(String [] arg)
+        {
+            int [] nums = { 1, 2, 3 , 5, 6 ,7};
+            Console.WriteLine(maxPermiter(nums));
+        }
+        public static int maxPermiter(int [] nums)
+        {
+            Array.Sort(nums);
+            Array.Reverse(nums);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i]< nums[i +1] + nums[i +2])
+                {
+                    return nums[i] + nums[i + 1] + nums[i + 2];
+                }
+            }
+            return 0;
+        }
+    }
+}
