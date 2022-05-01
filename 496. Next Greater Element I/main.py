810fbe890342ccdbf0ff90c58fc6a53673968725
ymost <ymostafa232@gmail.com>
2022-05-01 23:45:50 +0200

    day 6

--- 496. Next Greater Element I/main.py
+++ 496. Next Greater Element I/main.py
@@ -1,7 +1,17 @@
 def nextGreaterElement(nums1: list[int], nums2: list[int]) -> list[int]:
-    output_list = []
-    myDict = dict()
-    for i in range(len(nums1)):
-        myDict[nums1[i]] = i
+    output = [-1] * len(nums1)
+    mydict = {n: i for i, n in enumerate(nums1)}
+    stack = []
+    for i in nums2:
+        while stack and i > stack[-1]:
+            val = stack.pop()
+            index = mydict[val]
+            output[index] = i
 
-    for j in nums2:
+        if i in mydict:
+            stack.append(i)
+
+    return output
+
+
+print(nextGreaterElement([4, 1, 2], [1, 3, 4, 2]))
