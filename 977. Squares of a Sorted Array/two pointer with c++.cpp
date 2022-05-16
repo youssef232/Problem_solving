27a6f2d4b8044e11b14effc0819e624901f5246e
ymost <ymostafa232@gmail.com>
2022-05-16 14:37:19 +0200

    day 2.2

---
+++ 977. Squares of a Sorted Array/two pointer with c++.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+# include <vector>
+using namespace std;
+
+int main(){
+
+}
+vector<int> sortedSquares(vector<int>& nums) {
+    int left = 0;
+    int right = nums.size() - 1;
+    vector<int> answer;
+    answer.assign(0, nums.size())
+    for (int i =nums.size() -1 ; i <=0 ; i--)
+    {
+        if(abs(nums[left]) > abs(nums[right])){
+            answer[i] = nums[left] * nums[left];
+            left++;
+        }else{
+            answer[i] = nums[right] * nums[right];
+            right--;
+        }
+    }
+    
+    return answer;
+}
